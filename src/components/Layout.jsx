b56749f2edbb71779f23d991f9b5ead54520402d
freyazjiner <jinerz77@gmail.com>
2025-11-25 12:58:58 -0500

    Home page and event page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
+import { Box } from "@mui/material";
 import Header from "./Header";
 import Footer from "./Footer";
-import Container from "@mui/material/Container";
 
 export default function Layout({ children }) {
-  const links = [
-    { label: "Home", href: "/" },
-    { label: "About", href: "/about" },
-    { label: "Events", href: "/events" },
-  ];
-
   return (
     <>
-      <Header links={links} />
-      <Container maxWidth="lg" sx={{ mt: 1.5, mb: 2, px: { xs: 1.5, sm: 2 } }}>
-        {children}
-      </Container>
+      <Header />
+      <Box component="main">{children}</Box>
       <Footer />
     </>
   );
